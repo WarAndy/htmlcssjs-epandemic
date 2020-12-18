d6a42dbae00ee279643eb10ddad3f7dc37e146af
warandy <awarren7737@gmail.com>
2020-12-18 14:51:43 +0330

    graphbuilder update

--- assets/js/graphbuilder.js
+++ assets/js/graphbuilder.js
@@ -1,31 +1,83 @@
-window.addEventListener('load', showGraph, false);
+window.addEventListener('load', showGraph, true);
 
-const params = [
-    { legend: 'nouveaux cas', color: 'red', count: 1000 },
-    { legend: 'morts', color: 'black', count: 100 },
-    { legend: 'guéris', color: 'green', count: 500 }
+const dataFromDB = [
+    { legend: 'nouveaux cas', color: 'red', nbr: 800 },
+    { legend: 'morts', color: 'black', nbr: 200 },
+    { legend: 'guéris', color: 'green', nbr: 400 },
+    { legend: 'en traitement', color: 'orange', nbr: 600 - 500 + 400 }
 ];
+const provinceStat = {
+    'antsiranana': [
+        { legend: 'nouveaux cas', color: 'red', nbr: 800 },
+        { legend: 'morts', color: 'black', nbr: 200 },
+        { legend: 'guéris', color: 'green', nbr: 400 },
+        { legend: 'en traitement', color: 'orange', nbr: 600 - 500 + 400 }
+    ],
+    'mahajanga': [
+        { legend: 'nouveaux cas', color: 'red', nbr: 800 },
+        { legend: 'morts', color: 'black', nbr: 200 },
+        { legend: 'guéris', color: 'green', nbr: 400 },
+        { legend: 'en traitement', color: 'orange', nbr: 600 - 500 + 400 }
+    ],
+    'antananarivo': [
+        { legend: 'nouveaux cas', color: 'red', nbr: 800 },
+        { legend: 'morts', color: 'black', nbr: 200 },
+        { legend: 'guéris', color: 'green', nbr: 400 },
+        { legend: 'en traitement', color: 'orange', nbr: 600 - 500 + 400 }
+    ],
+    'toamasina': [
+        { legend: 'nouveaux cas', color: 'red', nbr: 800 },
+        { legend: 'morts', color: 'black', nbr: 200 },
+        { legend: 'guéris', color: 'green', nbr: 400 },
+        { legend: 'en traitement', color: 'orange', nbr: 600 - 500 + 400 }
+    ],
+    'fianarantsoa': [
+        { legend: 'nouveaux cas', color: 'red', nbr: 800 },
+        { legend: 'morts', color: 'black', nbr: 200 },
+        { legend: 'guéris', color: 'green', nbr: 400 },
+        { legend: 'en traitement', color: 'orange', nbr: 600 - 500 + 400 }
+    ],
+    'toliara': [
+        { legend: 'nouveaux cas', color: 'red', nbr: 800 },
+        { legend: 'morts', color: 'black', nbr: 200 },
+        { legend: 'guéris', color: 'green', nbr: 400 },
+        { legend: 'en traitement', color: 'orange', nbr: 600 - 500 + 400 }
+    ]
+};
 
 function showGraph() {
-    initGraph(document.querySelector('#chart'), params, true, "bilan quotidien");
+    const section_province = document.querySelector('#province_section');
+    for (const province in provinceStat) {
+        if (provinceStat.hasOwnProperty(province)) {
+            const element = provinceStat[province];
+            let canvas_for_province = document.createElement('canvas');
+            canvas_for_province.className = 'col-md-4';
+            section_province.appendChild(canvas_for_province);
+            initGraph(canvas_for_province,
+                element,
+                true, province.toUpperCase(), 'pie')
+        }
+    }
+    let now = new Date();
+    initGraph(document.querySelector('#chart'), dataFromDB, true, "bilan du " + now.getDay() + "-" + now.getMonth() + "-" + now.getFullYear(), 'bar');
 }
 
-function initGraph(myCanvas, params, withLegend = true, title = "") {
-    let data = new Array();
-    let legendes = new Array();
+function initGraph(myCanvas, data_, withLegend = true, title = "", chart_type) {
+    let count = new Array();
+    let legends = new Array();
     let colors = new Array();
-    params.forEach(element => {
-        data.push(element.count);
-        legendes.push(element.legend);
+    data_.forEach(element => {
+        count.push(element.nbr);
+        legends.push(element.legend);
         colors.push(element.color);
     });
-    let graph = new Chart(myCanvas, {
-        type: "pie",
-        params: {
-            labels: withLegend ? legendes : [],
-            paramssets: [{
+    let chrt = new Chart(myCanvas, {
+        type: chart_type,
+        data: {
+            labels: withLegend ? legends : [],
+            datasets: [{
                 label: title,
-                params: data,
+                data: count,
                 backgroundColor: colors,
                 borderWidth: 2,
                 borderColor: "#007bff",
@@ -41,9 +93,9 @@ function initGraph(myCanvas, params, withLegend = true, title = "") {
 
             },
             legend: {
-                position: "left",
+                position: "right",
                 labels: {
-                    fontColor: "black"
+                    fontColor: "white"
                 }
             }
         }
